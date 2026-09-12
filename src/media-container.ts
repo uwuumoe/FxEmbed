276fd607cf7593f1ace8a1a9271bdf8abe28a59c
Ashley <me@ashley.moe>
2026-09-12 04:22:27 -0600

    fix: preserve legacy media container class

--- src/media-container.ts
+++ src/media-container.ts
@@ -1,5 +1,10 @@
 import { Container } from '@cloudflare/containers';
 
+export class MediaTranscoder extends Container {
+  defaultPort = 8787;
+  sleepAfter = '10m';
+}
+
 export class MediaTranscoderV2 extends Container {
   defaultPort = 8787;
   sleepAfter = '10m';
